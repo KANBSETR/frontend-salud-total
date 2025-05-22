1efc3301de2c01255a1a043d001ed7613d3f3591
KANBSETR <azobisiso576@gmail.com>
2025-05-22 18:42:06 -0400

    Implementación de la API

--- src/app/services/especialidad.service.ts
+++ src/app/services/especialidad.service.ts
@@ -19,7 +19,7 @@ export interface Medico {
   providedIn: 'root'
 })
 export class EspecialidadService {
-  private baseUrl = 'api';
+  private baseUrl = 'https://api.nicodia.dev';
 
   async getEspecialidades(): Promise<Especialidad[]> {
     const response = await fetch(`${this.baseUrl}/especialidades`);
